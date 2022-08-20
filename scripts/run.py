016d4579863b4701b16b15c9fae315c444abf4d4
Avaer Kazmer <a@modules.io>
2022-08-20 06:41:38 -0400

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -52,6 +52,201 @@ def load_model_from_config(config, ckpt, verbose=False):
     model.eval()
     return model
 
+#
+# load model
+#
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--prompt",
+    type=str,
+    nargs="?",
+    default="a painting of a virus monster playing guitar",
+    help="the prompt to render"
+)
+parser.add_argument(
+    "--outdir",
+    type=str,
+    nargs="?",
+    help="dir to write results to",
+    default="outputs/txt2img-samples"
+)
+parser.add_argument(
+    "--skip_grid",
+    action='store_true',
+    help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
+)
+parser.add_argument(
+    "--skip_save",
+    action='store_true',
+    help="do not save individual samples. For speed measurements.",
+)
+parser.add_argument(
+    "--ddim_steps",
+    type=int,
+    default=50,
+    help="number of ddim sampling steps",
+)
+parser.add_argument(
+    "--plms",
+    action='store_true',
+    help="use plms sampling",
+)
+parser.add_argument(
+    "--laion400m",
+    action='store_true',
+    help="uses the LAION400M model",
+)
+parser.add_argument(
+    "--fixed_code",
+    action='store_true',
+    help="if enabled, uses the same starting code across samples ",
+)
+parser.add_argument(
+    "--ddim_eta",
+    type=float,
+    default=0.0,
+    help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+)
+parser.add_argument(
+    "--n_iter",
+    type=int,
+    default=2,
+    help="sample this often",
+)
+parser.add_argument(
+    "--H",
+    type=int,
+    default=512,
+    help="image height, in pixel space",
+)
+parser.add_argument(
+    "--W",
+    type=int,
+    default=512,
+    help="image width, in pixel space",
+)
+parser.add_argument(
+    "--C",
+    type=int,
+    default=4,
+    help="latent channels",
+)
+parser.add_argument(
+    "--f",
+    type=int,
+    default=8,
+    help="downsampling factor",
+)
+parser.add_argument(
+    "--n_samples",
+    type=int,
+    default=3,
+    help="how many samples to produce for each given prompt. A.k.a. batch size",
+)
+parser.add_argument(
+    "--n_rows",
+    type=int,
+    default=0,
+    help="rows in the grid (default: n_samples)",
+)
+parser.add_argument(
+    "--scale",
+    type=float,
+    default=7.5,
+    help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+)
+parser.add_argument(
+    "--from-file",
+    type=str,
+    help="if specified, load prompts from this file",
+)
+parser.add_argument(
+    "--config",
+    type=str,
+    default="configs/stable-diffusion/v1-inference.yaml",
+    help="path to config which constructs model",
+)
+parser.add_argument(
+    "--ckpt",
+    type=str,
+    default="models/ldm/stable-diffusion-v1/model.ckpt",
+    help="path to checkpoint of model",
+)
+parser.add_argument(
+    "--seed",
+    type=int,
+    default=42,
+    help="the seed (for reproducible sampling)",
+)
+parser.add_argument(
+    "--precision",
+    type=str,
+    help="evaluate at this precision",
+    choices=["full", "autocast"],
+    default="autocast"
+)
+
+opt = parser.parse_args([
+    '--prompt',
+    'anime girl',
+    '--plms',
+    '--ckpt',
+    'stable-diffusion-v-1-3/sd-v1-3-full-ema.ckpt',
+    '--n_samples',
+    '1',
+    '--skip_grid',
+])
+
+if opt.laion400m:
+    print("Falling back to LAION 400M model...")
+    opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
+    opt.ckpt = "models/ldm/text2img-large/model.ckpt"
+    opt.outdir = "outputs/txt2img-samples-laion400m"
+
+seed_everything(opt.seed)
+
+config = OmegaConf.load(f"{opt.config}")
+model = load_model_from_config(config, f"{opt.ckpt}")
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model = model.to(device)
+
+if opt.plms:
+    sampler = PLMSSampler(model)
+else:
+    sampler = DDIMSampler(model)
+
+os.makedirs(opt.outdir, exist_ok=True)
+outpath = opt.outdir
+
+batch_size = opt.n_samples
+n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
+if not opt.from_file:
+    prompt = s
+    assert prompt is not None
+    data = [batch_size * [prompt]]
+
+else:
+    print(f"reading prompts from {opt.from_file}")
+    with open(opt.from_file, "r") as f:
+        data = f.read().splitlines()
+        data = list(chunk(data, batch_size))
+
+sample_path = os.path.join(outpath, "samples")
+os.makedirs(sample_path, exist_ok=True)
+base_count = len(os.listdir(sample_path))
+grid_count = len(os.listdir(outpath)) - 1
+
+start_code = None
+if opt.fixed_code:
+    start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
+
+#
+# request handler
+#
+
 @app.route("/image")
 def home():
     s = request.args.get("s")
@@ -59,193 +254,6 @@ def home():
     if s is None or s == "":
         response = make_response("no text provided", 400)
     else:
-        # def main():
-        parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            "--prompt",
-            type=str,
-            nargs="?",
-            default="a painting of a virus monster playing guitar",
-            help="the prompt to render"
-        )
-        parser.add_argument(
-            "--outdir",
-            type=str,
-            nargs="?",
-            help="dir to write results to",
-            default="outputs/txt2img-samples"
-        )
-        parser.add_argument(
-            "--skip_grid",
-            action='store_true',
-            help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
-        )
-        parser.add_argument(
-            "--skip_save",
-            action='store_true',
-            help="do not save individual samples. For speed measurements.",
-        )
-        parser.add_argument(
-            "--ddim_steps",
-            type=int,
-            default=50,
-            help="number of ddim sampling steps",
-        )
-        parser.add_argument(
-            "--plms",
-            action='store_true',
-            help="use plms sampling",
-        )
-        parser.add_argument(
-            "--laion400m",
-            action='store_true',
-            help="uses the LAION400M model",
-        )
-        parser.add_argument(
-            "--fixed_code",
-            action='store_true',
-            help="if enabled, uses the same starting code across samples ",
-        )
-        parser.add_argument(
-            "--ddim_eta",
-            type=float,
-            default=0.0,
-            help="ddim eta (eta=0.0 corresponds to deterministic sampling",
-        )
-        parser.add_argument(
-            "--n_iter",
-            type=int,
-            default=2,
-            help="sample this often",
-        )
-        parser.add_argument(
-            "--H",
-            type=int,
-            default=512,
-            help="image height, in pixel space",
-        )
-        parser.add_argument(
-            "--W",
-            type=int,
-            default=512,
-            help="image width, in pixel space",
-        )
-        parser.add_argument(
-            "--C",
-            type=int,
-            default=4,
-            help="latent channels",
-        )
-        parser.add_argument(
-            "--f",
-            type=int,
-            default=8,
-            help="downsampling factor",
-        )
-        parser.add_argument(
-            "--n_samples",
-            type=int,
-            default=3,
-            help="how many samples to produce for each given prompt. A.k.a. batch size",
-        )
-        parser.add_argument(
-            "--n_rows",
-            type=int,
-            default=0,
-            help="rows in the grid (default: n_samples)",
-        )
-        parser.add_argument(
-            "--scale",
-            type=float,
-            default=7.5,
-            help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-        )
-        parser.add_argument(
-            "--from-file",
-            type=str,
-            help="if specified, load prompts from this file",
-        )
-        parser.add_argument(
-            "--config",
-            type=str,
-            default="configs/stable-diffusion/v1-inference.yaml",
-            help="path to config which constructs model",
-        )
-        parser.add_argument(
-            "--ckpt",
-            type=str,
-            default="models/ldm/stable-diffusion-v1/model.ckpt",
-            help="path to checkpoint of model",
-        )
-        parser.add_argument(
-            "--seed",
-            type=int,
-            default=42,
-            help="the seed (for reproducible sampling)",
-        )
-        parser.add_argument(
-            "--precision",
-            type=str,
-            help="evaluate at this precision",
-            choices=["full", "autocast"],
-            default="autocast"
-        )
-
-        opt = parser.parse_args([
-            '--prompt',
-            s,
-            '--plms',
-            '--ckpt',
-            'stable-diffusion-v-1-3/sd-v1-3-full-ema.ckpt',
-            '--n_samples',
-            '1',
-        ])
-
-        if opt.laion400m:
-            print("Falling back to LAION 400M model...")
-            opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
-            opt.ckpt = "models/ldm/text2img-large/model.ckpt"
-            opt.outdir = "outputs/txt2img-samples-laion400m"
-
-        seed_everything(opt.seed)
-
-        config = OmegaConf.load(f"{opt.config}")
-        model = load_model_from_config(config, f"{opt.ckpt}")
-
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        model = model.to(device)
-
-        if opt.plms:
-            sampler = PLMSSampler(model)
-        else:
-            sampler = DDIMSampler(model)
-
-        os.makedirs(opt.outdir, exist_ok=True)
-        outpath = opt.outdir
-
-        batch_size = opt.n_samples
-        n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-        if not opt.from_file:
-            prompt = opt.prompt
-            assert prompt is not None
-            data = [batch_size * [prompt]]
-
-        else:
-            print(f"reading prompts from {opt.from_file}")
-            with open(opt.from_file, "r") as f:
-                data = f.read().splitlines()
-                data = list(chunk(data, batch_size))
-
-        sample_path = os.path.join(outpath, "samples")
-        os.makedirs(sample_path, exist_ok=True)
-        base_count = len(os.listdir(sample_path))
-        grid_count = len(os.listdir(outpath)) - 1
-
-        start_code = None
-        if opt.fixed_code:
-            start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
-
         precision_scope = autocast if opt.precision=="autocast" else nullcontext
         with torch.no_grad():
             with precision_scope("cuda"):
